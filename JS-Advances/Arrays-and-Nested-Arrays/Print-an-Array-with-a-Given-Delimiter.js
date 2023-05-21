0de9c0bab3f2269efb0c914877e931fd2fab9812
SilviyaKolchakova <silviyahristowa@gmail.com>
2023-05-21 14:30:54 +0300

    demo file deleted

---
+++ JS-Advances/Arrays-and-Nested-Arrays/Print-an-Array-with-a-Given-Delimiter.js
@@ -0,0 +1,4 @@
+function print(collectionOfWords, delimiter) {
+  const output = collectionOfWords.join(delimiter);
+  console.log(output);
+}
--- demo.js
+++
@@ -1,4 +0,0 @@
-function print(collectionOfWords, delimeter) {
-  const output = collectionOfWords.join(delimeter);
-  console.log(output);
-}
